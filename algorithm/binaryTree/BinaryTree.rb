1cdf667cb25631a16efdf3c1c69f8636ff172d31
poppy <horpoppy@gmail.com>
2017-09-18 19:18:13 +0800

    哈夫曼编码及简单应用实现

--- algorithm/binaryTree/BinaryTree.rb
+++ algorithm/binaryTree/BinaryTree.rb
@@ -40,6 +40,15 @@ module BinaryTree
       (not @left and not @right)
     end
 
+    def left?
+       (@parent and @parent.left == self)
+    end
+
+    def right?
+       (@parent and @parent.rifht == self)
+    end
+       
+
   end   #the end of Node
 
   class  Tree
@@ -87,5 +96,104 @@ module BinaryTree
      return result
     end
   end  #the end of Tree
+
+  class Huffman
+    attr_accessor :huff
+    def initialize(tree = nil)
+      @huff = tree
+    end
+    def self.create_by_str(str)
+      #@huffman_str = "aaaaabbbbbbbbbbbbbbbbbbbbbbbbbbbbbcccccccddddddddeeeeeeeeeeeeeefffffffffffffffffffffffggghhhhhhhhhhh"
+      hash_huff = Hash.new(0)
+      
+      str.each_char do |ch|
+        hash_huff[ch] += 1
+      end
+      
+      create_by_hash(hash_huff)
+    end
+    def self.create_by_hash(hash_huff)
+      tempAry = []
+      
+      hash_huff.each do |key,value|
+        tempAry << BinaryTree::Node.new(value,key)
+      end
+
+      create_by_node_ary(tempAry)
+
+    end
+
+    def self.create_by_node_ary(tempAry)
+      while tempAry.size > 1
+        tempAry.sort! do |a,b|
+          a.value <=> b.value
+        end
+        left = tempAry[0]
+        right = tempAry[1]
+        tempTree = BinaryTree::Node.new(left.value + right.value)
+        tempTree.left = left
+        tempTree.right = right
+        left.parent = tempTree
+        right.parent = tempTree
+      
+        tempAry.delete(left)
+        tempAry.delete(right)
+      
+        tempAry << tempTree
+      end
+      @huff = tempAry[0]
+      return self
+    end
+
+    def self.encode_hash(order = :preorder)
+      raise StandardError.new "unexpected order,must select in :preorder,:inorder,:postorder " if not [:preorder,:inorder,:postorder].include?(order)
+      result = Hash.new("") 
+      @huff.visit(order) do |node|
+        if node.symbol
+          tempstr = ""
+          temp = node
+          while temp.parent
+            if temp.parent.right == temp
+              tempstr += "1"
+            else
+              tempstr += "0"
+            end
+            temp = temp.parent
+          end
+            result[node.symbol] =  tempstr.reverse!
+        end
+      end
+      return result
+    end
+
+    def self.decode_str(str)
+      result = ""
+      
+      decode_hash = Hash.new("")
+      encode_hash.each do |key,value|
+        decode_hash[value] = key
+      end
+      tempstr = ""
+      str.each_char do |ch|
+        tempstr += ch
+        if decode_hash.has_key? tempstr
+          result += decode_hash[tempstr]
+          tempstr = ""
+        end
+      end
+      return result
+
+    end
+
+    def self.encode_str(str)
+      the_hash = encode_hash
+      result = ""
+      str.each_char do |ch|
+        result += the_hash[ch]
+      end
+      return result
+    end
+
+  end
 
 end
--- algorithm/binaryTree/test_tree.rb
+++ algorithm/binaryTree/test_tree.rb
@@ -5,27 +5,117 @@ require './BinaryTree'
 include BinaryTree
 
 @new_tree = BinaryTree::Node.new('^')
-@new_tree =  BinaryTree::Tree.create_by_str(@default_tree,false)
-puts "广义表原型"
-puts @default_tree
-puts "先序遍历结果："
-@new_tree.visit(:preorder) do |node|
-  print "#{node.value} "
-end
-puts ""
-puts "中序遍历结果："
-@new_tree.visit(:inorder) do |node|
-  print "#{node.value} "
-end
-puts ""
-puts "后序遍历结果："
-@new_tree.visit(:postorder) do |node|
-  print "#{node.value} "
-end
-puts ""
+#@new_tree =  BinaryTree::Tree.create_by_str(@default_tree,false)
+#puts "广义表原型"
+#puts @default_tree
+#puts "先序遍历结果："
+#@new_tree.visit(:preorder) do |node|
+#  print "#{node.value} "
+#end
+#puts ""
+#puts "中序遍历结果："
+#@new_tree.visit(:inorder) do |node|
+#  print "#{node.value} "
+#end
+#puts ""
+#puts "后序遍历结果："
+#@new_tree.visit(:postorder) do |node|
+#  print "#{node.value} "
+#end
+#puts ""
 
-#@t = BinaryTree::Node.new('^')
-#@p = BinaryTree::Node.new('^')
+#@huffman_str = "aaaaabbbbbbbbbbbbbbbbbbbbbbbbbbbbbcccccccddddddddeeeeeeeeeeeeeefffffffffffffffffffffffggghhhhhhhhhhh"
+#
+#@hash_huff = Hash.new(0)
+#
+#@huffman_str.each_char do |ch|
+#  @hash_huff[ch] += 1
+#end
+#
+#tempAry = []
+#
+#@hash_huff.each do |key,value|
+#  tempAry << BinaryTree::Node.new(value,key)
+#end
+#
+#while tempAry.size > 1
+#  tempAry.sort! do |a,b|
+#    a.value <=> b.value
+#  end
+#  left = tempAry[0]
+#  right = tempAry[1]
+#  tempTree = BinaryTree::Node.new(left.value + right.value)
+#  tempTree.left = left
+#  tempTree.right = right
+#  left.parent = tempTree
+#  right.parent = tempTree
+#
+#  tempAry.delete(left)
+#  tempAry.delete(right)
+#
+#  tempAry << tempTree
+#end
+#
+#tree = tempAry[0]
+
+
+@huffman_str = "aaaaabbbbbbbbbbbbbbbbbbbbbbbbbbbbbcccccccddddddddeeeeeeeeeeeeeefffffffffffffffffffffffggghhhhhhhhhhh"
+
+@str_to_decode = "0001000100010001000110101010101010101010101010101010101010101010101010101010101110111011101110111011101110111111111111111111111111111111111101101101101101101101101101101101101101100101010101010101010101010101010101010101010101000000000000001001001001001001001001001001001"
+huffmanTree = BinaryTree::Huffman.create_by_str(@huffman_str)
+
+puts huffmanTree.encode_str(@huffman_str)
+
+puts huffmanTree.decode_str(@str_to_decode)
+
+#result =  huffmantree.encode_hash
+#@huffman_str.each_char do |ch|
+#  print result[ch]
+#end
+#
+#decode_hash = hash.new("")
+#result.each do |key,value|
+#  decode_hash[value] = key
+#end
+#tempstr = ""
+#@str_to_decode.each_char do |ch|
+#  tempstr += ch
+#  if decode_hash.has_key? tempstr
+#    print decode_hash[tempstr]
+#    tempstr = ""
+#  end
+#end
+#puts ""
+#puts result
+#huffmantree.visit(:preorder) do |node|
+#  if node.symbol
+#    print "#{node.symbol}:"
+#    tempstr = ""
+#    temp = node
+#    while temp.parent
+#      if temp.parent.right == temp
+#        tempstr += "1"
+#      else
+#        tempstr += "0"
+#      end
+#      temp = temp.parent
+#    end
+#    puts tempstr.reverse!
+#  end
+#end
+
+#tempary.each do |i|
+#  puts i.value
+#end
+
+#while temp.size > 1
+#  temptree = binarytree::node.new('^')
+#  temptree.left = binarytree::node.new(temp[0][0],temp[0][1],nil,nil,temptree)
+#  temptree.right = binarytree::node.new(temp[1][0],temp[1][1],nil,nil,temptree)
+#end
+
+#@t = binarytree::node.new('^')
+#@p = binarytree::node.new('^')
 #@k = 0
 #
 #tempAry = []
@@ -56,3 +146,4 @@ puts ""
 #      end
 #    end
 #end
+
