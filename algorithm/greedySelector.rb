be69cd4359d63d2cc05b6200ec2f542a1e9c914f
po-ppy <horpoppy@gmail.com>
2017-09-22 00:08:04 +0800

    会场安排问题(可当作业提交)

--- algorithm/greedySelector.rb
+++ algorithm/greedySelector.rb
@@ -1,11 +1,22 @@
 #_*_ coding:utf-8 _*_
 
+class Meeting
+  attr_accessor :beginTime,:endTime,:id
+  def initialize(id,beginTime,endTime)
+    @id,@beginTime,@endTime = id,beginTime,endTime
+  end
+end
+
 @begin = [1,3,0,5,3,5,6,8,8,2,12]
 @end = [4,5,6,7,8,9,10,11,12,13,14]
 @ary = Array.new(11,false)
-#@result = []
+@meetings = []
 
-def greedySelector(beginAry,endAry,ary)
+@end.each_with_index do |item,index|
+  @meetings << Meeting.new(index,@begin[index],item) #将会议放放到数组中
+end
+
+def greedySelector(beginAry,endAry,ary) #放进的数组是要按结束时间拍好序的
   ary[0] = true
   j,i = 0,1
   while i < beginAry.size
@@ -17,6 +28,29 @@ def greedySelector(beginAry,endAry,ary)
     end
     i += 1
   end
+end
+
+def greedySelector2(meetingAry)
+  meetingAry.sort_by! do |a|
+    a.endTime
+  end
+  selectAry = Array.new(meetingAry.size,false)
+  selectAry[0] = true
+  j,i = 0,1
+  while i < meetingAry.size
+    if meetingAry[i].beginTime > meetingAry[j].endTime
+      selectAry[i] = true
+      j = i
+    else
+      selectAry[i] = false
+    end
+    i += 1
+  end
+  resultAry = []
+  selectAry.each_with_index do |yes,index|
+    resultAry << meetingAry[index] if yes
+  end
+  return resultAry
 end
 
 #def greedySelector2(beginAry,endAry)
@@ -48,5 +82,11 @@ greedySelector(@begin,@end,@ary)
   if item
     print "#{index} "
   end
+end
+puts ""
+
+puts "自己修改的方法，结果如下:"
+greedySelector2(@meetings).each do |item|
+  print "#{item.id} "
 end
 puts ""
