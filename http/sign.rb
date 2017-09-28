3e65119ae4b67c780ec32da111e25e657e4bae12
po-ppy <horpoppy@gmail.com>
2017-09-28 21:14:56 +0800

    西农易班自动签到脚本雏形(待实战测试)

---
+++ http/sign.rb
@@ -0,0 +1,26 @@
+# _*_ coding:utf-8 _*_
+require 'mechanize'
+require 'net/http'
+require './name_ybid'
+
+#name_ybid = {'姓名' => 'ybid'}
+def sign(name_id_hash)
+  name_id_hash.each do |key,value|
+    agent = Mechanize.new
+    page = agent.get('http://kqapp.nwsuaf.edu.cn/ybcheck1.aspx?model=0&xsid='+value+'&sch=10712&token=1ea5edf1b56993f18c2ce93f5aaacd3ad9fd4c75&ts='+Time.now.to_i.to_s+'345')
+    planId = page.body.match(/GlbVar\.PlanId=.*;/).to_s.split("'")[1]
+    
+    if planId.size == 32
+      uri = URI('http://kqapp.nwsuaf.edu.cn/signing.ashx')
+        params = ['ZoneLng'=>'108.07843700000', 'ZoneLat'=>'34.29188800000', 'ZoneRadius'=>'620.00','StartTime'=>Time.now.strftime("%Y%m%d")+' 17:00','EndTime'=>Time.now.strftime("%Y%m%d")+' 19:30' ,'UserLng'=>'108.07844700000','UserLat'=>'34.29189800000','StudentId'=>value,'PlanId'=>planId,'SchoolCode'=>'10712']
+        puts "#{key}的签到返回结果如下："
+        res = Net::HTTP.post_form(uri,params)
+        puts res.body
+    else
+      puts "PlanId.size != 32  !!!!!!!!!!!"
+    end
+  end
+end
+
+#待实战测试
+sign(@name_ybid)
